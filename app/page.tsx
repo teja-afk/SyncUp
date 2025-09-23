b2868656c289da1f244e38cdf2dabed79497eca1
Teja Poosa <tejapoosa123@gmail.com>
2025-09-23 20:36:02 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
+import HowItWorksSection from '@/app/components/landing/HowItWorksSection';
+import FeaturesSection from './components/landing/FeaturesSection';
 import HeroSection from "./components/landing/HeroSection";
+import IntegrationsSection from './components/landing/IntegrationsSection';
 
 export default function Home() {
   return (
     <div className = "min-h-screen bg-black">
       <HeroSection />
+      <FeaturesSection/>
+      <IntegrationsSection />
+      <HowItWorksSection />
     </div>
   );
 }
